a74d359f6aceef2cb0bfde25a1a8d29413b71cd8
Sutanrrier <pabloryanbr0@gmail.com>
2023-02-08 11:17:47 -0300

    Sistema de pontuação finalizado

--- oj-gameshow/src/components/Sidebar.jsx
+++ oj-gameshow/src/components/Sidebar.jsx
@@ -3,8 +3,9 @@ import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
 import { useState } from "react";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
 
 function Sidebar() {
   const [jogadores, setJogadores] = useState([
@@ -17,6 +18,38 @@ function Sidebar() {
     { id: 7, nome: "Dreigel", pontos: 0 },
     { id: 8, nome: "Voltjedi", pontos: 0 },
   ]);
+
+  function handleAddPointClick(id) {
+    const jogadoresAux = [...jogadores];
+    jogadoresAux[id - 1].pontos += 1;
+
+    let jogadoresOrganizados = jogadoresAux.sort((j1, j2) =>
+      j1.pontos < j2.pontos ? 1 : j1.pontos > j2.pontos ? -1 : 0
+    );
+
+    for (let i = 0; i < jogadoresOrganizados.length; i++) {
+      jogadoresOrganizados[i].id = i + 1;
+    }
+
+    setJogadores(jogadoresOrganizados);
+  }
+
+  function handleRemovePointClick(id) {
+    const jogadoresAux = [...jogadores];
+    if (jogadoresAux[id - 1].pontos != 0) {
+      jogadoresAux[id - 1].pontos -= 1;
+    }
+
+    let jogadoresOrganizados = jogadoresAux.sort((j1, j2) =>
+      j1.pontos < j2.pontos ? 1 : j1.pontos > j2.pontos ? -1 : 0
+    );
+
+    for (let i = 0; i < jogadoresOrganizados.length; i++) {
+      jogadoresOrganizados[i].id = i + 1;
+    }
+
+    setJogadores(jogadoresOrganizados);
+  }
 
   return (
     <Drawer
@@ -41,14 +74,18 @@ function Sidebar() {
       <Box sx={{ width: 250 }}>
         <List sx={{ marginTop: 4 }}>
           {jogadores.map((jogador) => (
-            <ListItem key={jogador.nome} disablePadding>
-              <ListItemText
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-                primary={jogador.nome + " - " + jogador.pontos}
+            <ListItem key={jogador.id}>
+              <AddCircleIcon
+                className="oj-add-pontos-icon"
+                onClick={() => handleAddPointClick(jogador.id)}
               />
+              <RemoveCircleIcon
+                className="oj-remove-pontos-icon"
+                onClick={() => handleRemovePointClick(jogador.id)}
+              />
+              <p className="oj-jogador-hud">
+                {jogador.nome + " - " + jogador.pontos}
+              </p>
             </ListItem>
           ))}
         </List>
