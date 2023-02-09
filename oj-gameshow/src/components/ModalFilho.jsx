dc22b6b1248b4901f8c2968655f6cbfde8c96a61
Sutanrrier <pabloryanbr0@gmail.com>
2023-02-09 08:58:41 -0300

    Melhorado sistema de botoes e perguntas

---
+++ oj-gameshow/src/components/ModalFilho.jsx
@@ -0,0 +1,70 @@
+import { Modal } from "@mui/material";
+import { Box } from "@mui/system";
+import { useState } from "react";
+
+function ModalFilho(props) {
+  const modalStyle = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 1100,
+    bgcolor: "#f4a460",
+    p: 4,
+  };
+  const [open, setOpen] = useState(false);
+  const [dicaOpen, setDicaOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+    setDicaOpen(false);
+  };
+  const handleDicaOpen = () => {
+    setDicaOpen(true);
+  };
+
+  return (
+    <>
+      <button className="oj-questao-botao" onClick={handleOpen}>
+        Revelar Pergunta
+      </button>
+      <Modal hideBackdrop open={open} onClose={handleClose}>
+        <Box sx={modalStyle}>
+          <h1 className="oj-questao-titulo">{props.categoria}</h1>
+          <p
+            style={props.categoria != "TRIVIA" ? { display: "none" } : null}
+            className="oj-questao-pergunta"
+          >
+            {props.pergunta}
+          </p>
+          <p
+            style={
+              props.categoria == "TRIVIA" && dicaOpen
+                ? null
+                : { display: "none" }
+            }
+            className="oj-questao-pergunta"
+          >
+            {props.dica}
+          </p>
+          <img
+            className="oj-questao-personagem"
+            style={props.categoria === "TRIVIA" ? { display: "none" } : null}
+            src={!dicaOpen ? props.pergunta : props.dica}
+          />
+          <button className="oj-questao-botao" onClick={handleDicaOpen}>
+            Revelar Dica
+          </button>
+          <button className="oj-questao-botao" onClick={handleClose}>
+            Fechar Questao
+          </button>
+        </Box>
+      </Modal>
+    </>
+  );
+}
+
+export default ModalFilho;
--- oj-gameshow/src/pages/App.jsx
+++ oj-gameshow/src/pages/App.jsx
@@ -1,14 +1,38 @@
 import CardButton from "../components/CardButton";
 import Sidebar from "../components/Sidebar";
 import Box from "@mui/material/Box";
-import { Grid } from "@mui/material";
+import { Grid, Modal } from "@mui/material";
+import { useState } from "react";
+import ModalFilho from "../components/ModalFilho";
+
+import Imagem01 from "../images/testeimagem.png";
 
 function App() {
+  const modalStyle = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 1100,
+    bgcolor: "#f4a460",
+    p: 4,
+  };
+
   const questoes = [
-    { id: 1, categoria: "TRIVIA" },
-    { id: 2, categoria: "TRIVIA" },
-    { id: 3, categoria: "TRIVIA" },
-    { id: 4, categoria: "TRIVIA" },
+    {
+      id: 1,
+      categoria: "TRIVIA",
+      pergunta: "Quantos modos de jogo existem atualmente? (v. 3.11)",
+      dica: "Dica teste",
+    },
+    {
+      id: 2,
+      categoria: "QUEM É ESSE PERSONAGEM",
+      pergunta: "https://i.imgur.com/d6s8F3G.png",
+      dica: "https://i.imgur.com/8y6HIrJ.jpg",
+    },
+    { id: 3, categoria: "EXPLORADOR DE MAPAS" },
+    { id: 4, categoria: "HERÓI MUSICAL" },
     { id: 5, categoria: "TRIVIA" },
     { id: 6, categoria: "TRIVIA" },
     { id: 7, categoria: "TRIVIA" },
@@ -30,12 +54,30 @@ function App() {
     { id: 23, categoria: "TRIVIA" },
     { id: 24, categoria: "TRIVIA" },
   ];
+  const [categoria, setCategoria] = useState("");
+  const [pergunta, setPergunta] = useState("");
+  const [dica, setDica] = useState("");
+  const [open, setOpen] = useState(false);
+  const handleOpen = (categoria, pergunta, dica) => {
+    setOpen(true);
+    setCategoria(categoria);
+    setPergunta(pergunta);
+    setDica(dica);
+  };
+
+  const handleClose = () => setOpen(false);
 
   return (
-    <Box sx={{ display: "flex", marginLeft: 5 }}>
+    <Box sx={{ display: "flex", margin: "4rem 4rem" }}>
       <Sidebar />
       <Box component="main">
         <h1 className="oj-game-title">100% OJ GAMESHOW</h1>
+        <Modal open={open} onClose={handleClose}>
+          <Box sx={modalStyle}>
+            <h1 className="oj-questao-titulo">{categoria}</h1>
+            <ModalFilho categoria={categoria} pergunta={pergunta} dica={dica} />
+          </Box>
+        </Modal>
         <Grid
           spacing={4}
           container
@@ -44,7 +86,14 @@ function App() {
         >
           {questoes.map((questao) => {
             return (
-              <Grid item md={1} key={questao.id}>
+              <Grid
+                item
+                md={1}
+                key={questao.id}
+                onClick={() =>
+                  handleOpen(questao.categoria, questao.pergunta, questao.dica)
+                }
+              >
                 <CardButton id={questao.id} />
               </Grid>
             );
